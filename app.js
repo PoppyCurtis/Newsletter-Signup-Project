24805993f4f4a55deb4f90114a8996915f5dc083
PoppyCurtis <poppy.curtis@gmail.com>
2019-03-13 12:10:47 +0000

    set up responses to signin fields using get and post requests

--- app.js
+++ app.js
@@ -15,6 +15,14 @@ app.get("/signup.html", function(req, res) {
 app.get("/signup.html", function(req, res) {
     res.sendFile(__dirname + "/failure.html");
 });
+app.post("/", function(req, res) {
+    var firstName = req.body.fName;
+    var secondName = req.body.sName;
+    var email = req.body.eMail;
+
+
+    console.log(firstName, secondName, email);
+});
 
 app.listen(3000, function() {
     console.log("Server is running on port 3000");
